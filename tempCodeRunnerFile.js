3a759b37ad740500a9aed50899898a4defdcb7c7
RAJ kr Rabidas <das752101@gmail.com>
2025-03-19 10:28:56 +0530

    Delete my_file.txt

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-let word = prompt("Enter a word");
-// let reversedWord = word.split('').reverse().join('');
-
-// if (word === reversedWord) {
-//     console.log("Palindrome");
-// } else {
-//     console.log("Not a Palindrome");
-// }
